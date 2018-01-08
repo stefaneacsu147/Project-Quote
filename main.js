2aa0e1cbd4ea68d6619a134693edddd6269c7814
stefaneacsu147 <stefaneacsu@gmail.com>
2018-01-09 01:07:18 +0200

    update v2

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-$(window).keypress(function(e) {
-      if (e.which === 32) {
-        $("#quote").addClass("reset");
-        $("#quote").removeClass("executed");
-        $("#writer").toggleClass("fade");
-        setTimeout(function(){
-          $.ajax({
-            crossOrigin: true,
-            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
-            dataType:"jsonp"
-        });
-        }, 1000);
-      }
-  });
-  function mycallback(json){
-    var quote = json[0];
-    $("#quote").html(quote.content)
-    $("#writer").html(quote.title)
-    $("#quote").addClass("executed");
-      $("#quote").removeClass("reset");
-      $("#writer").toggleClass("fade");
+$(window).keypress(function (e) {
+  if (e.which === 32) {
+    $("#quote").addClass("reset");
+    $("#quote").removeClass("executed");
+    $("#writer").toggleClass("fade");
+    setTimeout(function () {
+      $.ajax({
+        crossOrigin: true,
+        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
+        dataType: "jsonp"
+      });
+    }, 1000);
   }
+});
 
-  var refreshRate = 10000;
+function mycallback(json) {
+  var quote = json[0];
+  $("#quote").html(quote.content)
+  $("#writer").html(quote.title)
+  $("#quote").addClass("executed");
+  $("#quote").removeClass("reset");
+  $("#writer").toggleClass("fade");
+}
 
-  setInterval(function()  {
-    window.location.reload();
-  }, refreshRate);
+var refreshRate = 10000;
 
-  $('.random-image').hide();
+setInterval(function () {
+  window.location.reload();
+}, refreshRate);
 
-  $('.random-image').css({
-    // serves a random image from this collection on Unsplash.com
-    'background-image' : 'url(https://source.unsplash.com/collection/142324/1600x900)'
-  }).fadeIn(1500);
+$('.random-image').hide();
+
+$('.random-image').css({
+  // serves a random image from this collection on Unsplash.com
+  'background-image': 'url(https://source.unsplash.com/collection/142324/1600x900)'
+}).fadeIn(1500);
